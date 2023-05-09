b36a5d4c182946cc9b776f3d5dcdeb9e267578ce
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-05-09 01:13:03 -0300

    Update OrdemAlfabetica.java

--- ListaExer4/src/ArrayList/OrdemAlfabetica.java
+++ ListaExer4/src/ArrayList/OrdemAlfabetica.java
@@ -1,5 +1,30 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class OrdemAlfabetica {
 
+	public static void main(String[] args) {
+		
+		ArrayList<String> lista = new ArrayList<String>();
+		String nome;
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			System.out.println("Digite um nome (quando não querer mais, digite [N]): " );
+			nome = sc.nextLine();
+			lista.add(nome);
+		}
+		while( nome != "N" || nome != "n"  ); //não quer parar, oq fazer ?
+		
+		Collections.sort(lista);
+		
+		for(String x : lista) {
+			System.out.println( x );
+		}
+    
+	}
+  
 }
