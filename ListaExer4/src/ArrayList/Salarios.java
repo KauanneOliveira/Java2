dedad852aa5bd00a14cde732ae29eb9132ca9071
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-05-09 00:55:06 -0300

    Update Salarios.java

--- ListaExer4/src/ArrayList/Salarios.java
+++ ListaExer4/src/ArrayList/Salarios.java
@@ -7,13 +7,11 @@ public class Salarios {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-				
+		Scanner sc = new Scanner(System.in);		
 		ArrayList<Double> lista = new ArrayList<Double>();
-		
 		Double valor = 0.0;
-		
 		Double reajuste = 0.0;
+		Double novoValor = 0.0;
 		
 		do {
 			System.out.println("Digite o salario (quando acabar digite -1): " );
@@ -25,12 +23,14 @@ public class Salarios {
 		
 		System.out.println("\n\nDigite o valor do reajuste: " );
 		reajuste = sc.nextDouble();
+		
+		System.out.println();		
+		reajuste = reajuste / 100;
 
-		
-		for(Double x: lista) {
-			System.out.println( "Salário ajustado: " +x );
-		}
-		
+		for (Double x : lista) {
+		    novoValor = x * reajuste;
+		    x += novoValor;
+		    System.out.println("Novo Salário: " +x);
+		}	
 	}
-
 }
